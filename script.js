79f798ecf9870b4180894d3e57bf8f9ba00e5049
Lucas Panadero <lucasn.panadero@gmail.com>
2023-12-02 15:47:04 -0300

    comite

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 'use strict';
 
-/*
- *  APELLIDOS
- */
-
 const d = document;
-
 const productos = [
   { id: 1, nombre: 'Producto1', descripcion: 'Descripción del producto', precio: 1, imagen: 'assets/product/AroundTheFur-sm.png', categoria: 'Remera' },
   { id: 2, nombre: 'Producto2', descripcion: 'Descripción del producto', precio: 2, imagen: 'assets/product/AroundTheFur-sm.png', categoria: 'Buzo' },
@@ -15,92 +10,81 @@ const productos = [
   { id: 6, nombre: 'Producto6', descripcion: 'Descripción del producto', precio: 6, imagen: 'assets/product/AroundTheFur-sm.png', categoria: 'Buzo' },
 ];
 
-// Objetos:
 const info = d.querySelector('#info-carrito');
 const filtros = d.querySelectorAll('#filtros a');
 const products = d.querySelector('#productos');
 const listaCarrito = d.getElementById('lista-carrito');
+const itemProducto = listaCarrito.children;
+const cantidadesElement = d.querySelectorAll('.cantidades-carrito');
+const totalElement = d.getElementById('total-carrito');
+const reset = d.querySelector('#reset');
+const modalBody = d.getElementById('productoModalBody');
 
-productos.forEach((producto) => {
+let carrito = { productosIds: [], cantidades: [], total: 0 };
+
+productos.forEach(producto => {
   const card = d.createElement('article');
   card.classList.add('card', 'text-center');
   products.append(card);
-
 
   const imagen = d.createElement('img');
   imagen.src = producto.imagen;
   imagen.alt = producto.nombre;
   imagen.classList.add('card-img-top');
   imagen.setAttribute('data-bs-toggle', 'modal');
-  imagen.setAttribute('data-bs-target','#productModal');
+  imagen.setAttribute('data-bs-target', '#productModal');
   imagen.addEventListener('click', () => detalleProducto(producto));
   card.append(imagen);
 
-  const categoria = d.createElement('p');
-  categoria.textContent = producto.categoria;
-  // card.appendChild(categoria);
-
   const titulo = d.createElement('h3');
   titulo.textContent = producto.nombre;
-  // card.appendChild(titulo);
-  
+  card.appendChild(titulo);
+
   const descripcion = d.createElement('p');
   descripcion.textContent = producto.descripcion;
-  // card.appendChild(descripcion);
-  
+  card.appendChild(descripcion);
+
+
   const precio = d.createElement('p');
   const precioSpan = d.createElement('span');
+  precio.classList.add('price');
   precioSpan.textContent = producto.precio;
-  precio.appendChild(d.createTextNode('Precio: $'));
+  precio.appendChild(d.createTextNode('$'));
   precio.appendChild(precioSpan);
   card.appendChild(precio);
-  
-  
-  let addBtn = d.createElement('button');
+
+  const addBtn = d.createElement('button');
   addBtn.classList.add('add');
   addBtn.dataset.id = producto.id;
   addBtn.dataset.val = producto.precio;
   addBtn.dataset.cat = producto.categoria;
   addBtn.innerHTML = 'Agregar al carrito';
-
   addBtn.addEventListener('click', () => agregarAlCarrito(producto));
   card.appendChild(addBtn);
-
 });
 
-let addBtns = d.querySelectorAll('.add');
+reset.addEventListener('click', () => {
+  carrito = { productosIds: [], cantidades: [], total: 0 };
+  while (listaCarrito.hasChildNodes()) {
+    listaCarrito.removeChild(listaCarrito.firstChild);
+  }
+  mostrarCarrito();
+});
 
-
-/* Carrito componentes */
-let itemProducto = listaCarrito.children;
-
-let cantidadGeneral = 0;
-let cantidadesIndividuales = {};
-
-/* Agregar item carrito */
- function agregarAlCarrito(producto) {
+function agregarAlCarrito(producto) {
   const id = producto.id;
   const val = producto.precio;
-
-  //Verificando si el producto ya está en el carrito
   const indiceId = carrito.productosIds.indexOf(id);
 
-  if(indiceId !== -1) {
-    // Sí existe, incrementar cantidad y no agregar nuevo item
+  if (indiceId !== -1) {
     carrito.cantidades[indiceId]++;
-    //Actualizar total al incremental cantidad
     carrito.total += val;
-
-    // Actualizar texto cantidad en carrito
     const cantidadElement = itemProducto[indiceId].querySelector('.cantidad-prod');
-    cantidadElement.textContent = `Cantidad: ${carrito.cantidades[indiceId]}`;
-    
+    cantidadElement.textContent = `${carrito.cantidades[indiceId]}`;
   } else {
-    // Sí no existe, agregar nuevo item al carrito
     carrito.productosIds.push(id);
     carrito.cantidades.push(1);
 
-    // Crear item en carrito
     const listItem = d.createElement('li');
     listItem.classList.add('item-producto');
     listaCarrito.appendChild(listItem);
@@ -114,28 +98,21 @@ let cantidadesIndividuales = {};
     miniportada.src = producto.imagen;
     miniportada.alt = producto.nombre;
     descripCar.appendChild(miniportada);
-    
 
     const tituloCar = d.createElement('h3');
     tituloCar.classList.add('titulo-car');
-
     const nombreProducto = d.createTextNode(producto.nombre);
     const precioProcucto = d.createTextNode(` $${producto.precio}`);
-
     const spanPrecio = d.createElement('span');
     spanPrecio.appendChild(precioProcucto);
-
     const cantidadProducto = d.createElement('span');
     cantidadProducto.classList.add('cantidad-prod');
-    cantidadProducto.textContent = `Cantidad: ${carrito.cantidades[carrito.productosIds.indexOf(producto.id)] || 0}`;
-
+    cantidadProducto.textContent = `${carrito.cantidades[carrito.productosIds.indexOf(producto.id)] || 0}`;
     tituloCar.appendChild(nombreProducto);
     tituloCar.appendChild(d.createElement('br'));
     tituloCar.appendChild(spanPrecio);
     tituloCar.appendChild(cantidadProducto);
-
     descripCar.appendChild(tituloCar);
-
 
     const delBtn = d.createElement('button');
     delBtn.classList.add('del');
@@ -144,211 +121,127 @@ let cantidadesIndividuales = {};
     delBtn.dataset.cat = producto.categoria;
     delBtn.innerHTML = 'x';
     listItem.appendChild(delBtn);
-    
-    
+
     delBtn.addEventListener('click', () => eliminarDelCarrito(listItem));
-    
-    // Actualizar el total
+
     carrito.total += val;
   }
-  
 
-  // Mostrar detalle del carrito
   mostrarCarrito();
 }
-
-
-let delBtns = d.querySelectorAll('.del');
 
 function eliminarDelCarrito(item) {
   const id = parseInt(item.querySelector('.del').dataset.id);
   const val = parseInt(item.querySelector('.del').dataset.val);
-
-  // Obtener el índice del producto en el carrito
   const indiceId = carrito.productosIds.indexOf(id);
 
   if (indiceId !== -1) {
-      // Restar el precio acumulado total
-      carrito.total -= carrito.cantidades[indiceId] * val;
-
-      // Eliminar el ID del producto del carrito y cantidad asociada
-      carrito.productosIds.splice(indiceId, 1);
-      carrito.cantidades.splice(indiceId, 1);
-
-      // Actualizar el carrito
-      listaCarrito.removeChild(item);
-      mostrarCarrito();
-
-      // Restar la cantidad eliminada al total de productos en el carrito
-      carrito.total -= cantidadEliminada;
-
-      // Actualizar la cantidad en tiempo real en todos los elementos con la clase 'cantidades-carrito'
-      const cantidadesElement = d.querySelectorAll('.cantidades-carrito');
-      cantidadesElement.forEach(element => {
-        element.textContent = carrito.cantidades.reduce((acum, n) => acum + n, 0);
-      });
+    carrito.total -= carrito.cantidades[indiceId] * val;
+    carrito.productosIds.splice(indiceId, 1);
+    carrito.cantidades.splice(indiceId, 1);
+    listaCarrito.removeChild(item);
+    mostrarCarrito();
   }
 }
 
-// Acción de los botones para quitar productos:
-for (let btn of delBtns) {
+for (let btn of d.querySelectorAll('.del')) {
   btn.addEventListener('click', (e) => {
     const item = e.target.closest('.item-producto');
     eliminarDelCarrito(item);
   });
 }
 
-
-let carrito = {
-    productosIds: [],
-    cantidades: [],
-    total: 0,
-};
-
-
 const mostrarCarrito = () => {
-    const cantidadesElement = d.querySelectorAll('.cantidades-carrito');
-    const totalElement = d.getElementById('total-carrito')
-    
-    // Muestro el detalle del carrito:
-    const total = carrito.cantidades.reduce((acum, cantidad, indice) => {
-      const producto = productos.find(p => p.id === carrito.productosIds[indice]);
-      if (producto) {
-        return acum + cantidad * producto.precio;
-      } else { 
-        return acum;
-      }
-    }, 0);
-    
-    // Actualizar numero de botón carrito
-    cantidadesElement.forEach(element => {
-      // cantidadesElement.textContent = `Tenes ${carrito.cantidades.reduce((acum, n) => acum + n, 0)} productos en el carrito.`;
-      element.textContent = carrito.cantidades.reduce((acum, n) => acum + n, 0);
-    })
-    totalElement.textContent = `$${total}`;
+  const total = carrito.cantidades.reduce((acum, cantidad, indice) => {
+    const producto = productos.find(p => p.id === carrito.productosIds[indice]);
+    return acum + (producto ? cantidad * producto.precio : 0);
+  }, 0);
+
+  cantidadesElement.forEach(element => {
+    element.textContent = carrito.cantidades.reduce((acum, n) => acum + n, 0);
+  });
+
+  totalElement.textContent = `$${total}`;
 };
 
-// Reseteo:
-let reset = d.querySelector('#reset');
-reset.addEventListener('click', () => {
-    carrito = {
-        productosIds: [],
-        cantidades: [],
-        total: 0,
-    };
-    cantidadGeneral = 0;
-    cantidadesIndividuales = {};
-
-    
-    while (listaCarrito.hasChildNodes()) {
-      listaCarrito.removeChild(listaCarrito.firstChild);
-    }
-    mostrarCarrito();
-});
-
-/* Modal */
 function detalleProducto(producto) {
-  const modalBody = d.getElementById('productoModalBody');
-
   while (modalBody.hasChildNodes()) {
     modalBody.removeChild(modalBody.firstChild);
   }
 
-  const imagen = d.createElement('img');
-  imagen.src = producto.imagen;
-  imagen.alt = producto.nombre;
-  modalBody.appendChild(imagen);
+  const elements = [
+    { tag: 'img', attributes: { src: producto.imagen, alt: producto.nombre } },
+    { tag: 'h2', textContent: producto.nombre, classList: ['modal-titulo'] },
+    { tag: 'p', textContent: producto.categoria, classList: ['badge', 'bg-secondary'] },
+    { tag: 'p', textContent: producto.descripcion },
+    { tag: 'p', textContent: `$${producto.precio}`, classList: ['price'] },
+    { tag: 'button', classList: ['add'], dataset: { id: producto.id, val: producto.precio, cat: producto.categoria }, innerHTML: 'Agregar al carrito', eventListener: () => agregarAlCarrito(producto) }
+  ];
 
-  const titulo = d.createElement('h2');
-  titulo.classList.add('modal-titulo');
-  titulo.textContent = producto.nombre;
-  modalBody.appendChild(titulo);
-
-  const categoria = d.createElement('p');
-  categoria.textContent = producto.categoria;
-  categoria.classList.add('badge','bg-secondary');
-  modalBody.appendChild(categoria);
-
-  const descripcion = d.createElement('p');
-  descripcion.textContent = producto.descripcion;
-  modalBody.appendChild(descripcion);
-
-  const precio = d.createElement('p');
-  precio.textContent = `$${producto.precio}`;
-  modalBody.appendChild(precio);
-
-  const addBtn = d.createElement('button');
-  addBtn.classList.add('add');
-  addBtn.dataset.id = producto.id;
-  addBtn.dataset.val = producto.precio;
-  addBtn.dataset.cat = producto.categoria;
-  addBtn.innerHTML = 'Agregar al carrito';
-
-  addBtn.addEventListener('click', () => agregarAlCarrito(producto));
-  modalBody.appendChild(addBtn);
+  elements.forEach(element => {
+    const el = d.createElement(element.tag);
+    if (element.textContent) el.textContent = element.textContent;
+    if (element.attributes) Object.entries(element.attributes).forEach(([key, value]) => el.setAttribute(key, value));
+    if (element.classList) el.classList.add(...element.classList);
+    if (element.dataset) Object.entries(element.dataset).forEach(([key, value]) => el.dataset[key] = value);
+    if (element.innerHTML) el.innerHTML = element.innerHTML;
+    if (element.eventListener) el.addEventListener('click', element.eventListener);
+    modalBody.appendChild(el);
+  });
 }
 
-// Mostrar carrito inicial:
-mostrarCarrito();
-
-/* Categorias */
 const categorias = d.querySelectorAll('.dropdown-item');
 
-categorias.forEach((categoria) => {
+categorias.forEach(categoria => {
   categoria.addEventListener('click', () => filtrarCat(categoria.dataset.cat));
-})
+});
 
 function filtrarCat(categoriaSelect) {
-  // Si la categoría seleccionada es "todo", mostrar todo
   const productosFiltrados = categoriaSelect === 'todo' ? productos : productos.filter(producto => producto.categoria === categoriaSelect);
 
   while (products.hasChildNodes()) {
     products.removeChild(products.firstChild);
   }
 
-  productosFiltrados.forEach((producto) => {
+  productosFiltrados.forEach(producto => {
     const card = d.createElement('article');
     card.classList.add('card', 'text-center');
     products.append(card);
-
 
     const imagen = d.createElement('img');
     imagen.src = producto.imagen;
     imagen.alt = producto.nombre;
     imagen.classList.add('card-img-top');
     imagen.setAttribute('data-bs-toggle', 'modal');
-    imagen.setAttribute('data-bs-target','#productModal');
+    imagen.setAttribute('data-bs-target', '#productModal');
     imagen.addEventListener('click', () => detalleProducto(producto));
     card.append(imagen);
 
-    const categoria = d.createElement('p');
-    categoria.textContent = producto.categoria;
-    // card.appendChild(categoria);
-
     const titulo = d.createElement('h3');
     titulo.textContent = producto.nombre;
-    // card.appendChild(titulo);
-    
+    card.appendChild(titulo);
+  
     const descripcion = d.createElement('p');
     descripcion.textContent = producto.descripcion;
-    // card.appendChild(descripcion);
-    
+    card.appendChild(descripcion);
+
     const precio = d.createElement('p');
     const precioSpan = d.createElement('span');
+    precio.classList.add('price');
     precioSpan.textContent = producto.precio;
-    precio.appendChild(d.createTextNode('Precio: $'));
+    precio.appendChild(d.createTextNode('$'));
     precio.appendChild(precioSpan);
-    // card.appendChild(precio);
-    
-    
+    card.appendChild(precio);
+
     let addBtn = d.createElement('button');
     addBtn.classList.add('add');
     addBtn.dataset.id = producto.id;
     addBtn.dataset.val = producto.precio;
     addBtn.dataset.cat = producto.categoria;
     addBtn.innerHTML = 'Agregar al carrito';
-
     addBtn.addEventListener('click', () => agregarAlCarrito(producto));
     card.appendChild(addBtn);
-  })
+  });
 }
+
+mostrarCarrito();
